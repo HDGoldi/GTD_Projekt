91730a7cfda437962f9298d06e2296335f9d9ad3
Sulaiman <jan.sulaiman@1nce.com>
2019-07-23 19:22:08 +0200

    added first visualizations

--- Anlaysis_2.R
+++ Anlaysis_2.R
@@ -6,3 +6,26 @@ glimpse(data)
 summary(data)
 library(DataExplorer)
 DataExplorer::create_report(data)
+library(FactoMineR)
+library("factoextra")
+res.pca <- PCA(data,  graph = FALSE)
+scatterplotMatrix(data)
+library(purrr)
+library(tidyr)
+library(ggplot2)
+data %>%
+  keep(is.numeric) %>% 
+  gather() %>% 
+  ggplot(aes(value)) +
+    facet_wrap(~ key, scales = "free") +
+    geom_histogram()
+library(ggpubr)
+ggbarplot(data, x = "country", y = "nkill",
+          fill = "cyl",               # change fill color by cyl
+          color = "white",            # Set bar border colors to white
+          palette = "jco",            # jco journal color palett. see ?ggpar
+          sort.val = "desc",          # Sort the value in dscending order
+          sort.by.groups = FALSE,     # Don't sort inside each group
+          x.text.angle = 90           # Rotate vertically x axis texts)
+
+          
