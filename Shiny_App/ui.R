66dc47f56ea1c842b17cd2a6e2ee253ea66f19eb
Jan Sulaiman <jan.sulaiman@1nce.com>
2019-08-07 21:39:31 +0200

    all to width 12 and moved from uni to multi some plots

--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -55,6 +55,8 @@ body <- dashboardBody(tabItems(
             h2("Overview & Table of Contents"),
             fluidRow(
                 box(
+                    width = 12,
+                    height = 200,
                     h2("Global-Terrorism Database"),
                     background = "light-blue",
                     solidHeader = TRUE,
@@ -76,6 +78,8 @@ body <- dashboardBody(tabItems(
                     
                 ),
                 box(
+                    width = 12,
+                    height = 200,
                     h2("Data Source"),
                     style = "font-size: 120%; background-color : #e77e7e;",
                     solidHeader = TRUE,
@@ -97,6 +101,7 @@ body <- dashboardBody(tabItems(
             h2("Preparation of Raw Data"),
             fluidRow(
                 tabBox(
+                    width = 12,
                     title = "Missing Values",
                     height = "200",
                     tabPanel("Original Dataset", h3("Total of 135 variables with a lot of missing values"), withSpinner(plotOutput('missingdata'))),
@@ -106,6 +111,7 @@ body <- dashboardBody(tabItems(
     tabItem(tabName = "page3",
             h2("Exploring the Dataset"),
             fluidRow(
+                width = 12,
                 valueBoxOutput("total_values"),
                 valueBoxOutput("total_years"),
                 valueBoxOutput("total_countries"),
@@ -115,96 +121,82 @@ body <- dashboardBody(tabItems(
             ),
             withSpinner(DT::dataTableOutput("datatable"))),
 
-    tabItem(tabName = "page4",
-            h2("Univariate Insights into Global Terror"),
-            fluidRow(box(width = 12,
-                         includeMarkdown("univariat.md")) 
-            ),
-            fluidRow(
-                box(width = 12, 
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot1"))
-                    ),
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot2"))
-                    ),
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot3"))
-                    ),
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot4"))
-                    )
-                )
-            ),
-            
-            fluidRow(box(width = 12,
-                         includeMarkdown("univariat2.md")) 
-            ),
-            fluidRow(
-                box(width = 12,
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot5"))
-                    ),
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot6"))
-                    ),
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot7"))
-                    ),
-                    column(width  = 3,
-                           withSpinner(plotOutput("boxplot8"))
-                    )
-                )
-            ),
-            
-            fluidRow(box(width = 12,
-                         includeMarkdown("univariat3.md")) 
-            ),
-            
-            fluidRow(
-                tabBox(
-                    title = "Casualty Count Distributions (Multivariat?)",
-                    width = "12",
-                    tabPanel("By Year",h3(""), withSpinner(plotlyOutput("casdist_year"))),
-                    tabPanel("By Region",h3(""), withSpinner(plotlyOutput("casdist_region"))),
-                    tabPanel("By Attack Type",h3(""), withSpinner(plotlyOutput("casdist_attack"))),
-                    tabPanel("By Weapon Type",h3(""), withSpinner(plotlyOutput("casdist_weap")))
-                )
-                
-            ),
-            
-            
-            fluidRow(valueBoxOutput("attack_year"),
-                     valueBoxOutput("casual_att")
-                     
-            ),
-            fluidRow(
-                tabBox(
-                    title = "Attack Count Distributions",
-                    width = "12",
-                    tabPanel("By Year",h3(""), withSpinner(plotlyOutput("atdist_year"))),
-                    tabPanel("By Region",h3(""), withSpinner(plotlyOutput("atdist_region"))),
-                    tabPanel("By Attack Type",h3(""), withSpinner(plotlyOutput("atdist_attack"))),
-                    tabPanel("By Weapon Type",h3(""), withSpinner(plotlyOutput("atdist_weap")))
-                )
-                
-            ),
-            fluidRow(
-                tabBox(
-                    title = "Multivariate?",
-                    width = "12",
-                    tabPanel("Attacks By Attacktype & Region", h3(""), withSpinner(plotlyOutput("dist_region2"))),
-                    tabPanel("Attacks By Region & Year", h3(""), withSpinner(plotlyOutput("distyear2")))
-                )
-                
-            )),
+    tabItem(
+        tabName = "page4",
+        h2("Univariate Insights into Global Terror"),
+        fluidRow(box(width = 12,
+                     includeMarkdown("univariat.md"))),
+        fluidRow(box(
+            width = 12,
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot1"))),
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot2"))),
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot3"))),
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot4")))
+        )),
+        
+        fluidRow(box(
+            width = 12,
+            includeMarkdown("univariat2.md")
+        )),
+        fluidRow(box(
+            width = 12,
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot5"))),
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot6"))),
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot7"))),
+            column(width  = 3,
+                   withSpinner(plotOutput("boxplot8")))
+        )),
+        
+        fluidRow(box(
+            width = 12,
+            includeMarkdown("univariat3.md")
+        )),
+    fluidRow(width = 12,
+             valueBoxOutput("attack_year"),
+             valueBoxOutput("casual_att")),
+    fluidRow(
+        tabBox(
+            title = "Attack Count Distributions",
+            width = 12,
+            tabPanel("By Year", h3(""), withSpinner(plotlyOutput("atdist_year"))),
+            tabPanel("By Region", h3(""), withSpinner(plotlyOutput("atdist_region"))),
+            tabPanel("By Attack Type", h3(""), withSpinner(plotlyOutput("atdist_attack"))),
+            tabPanel("By Weapon Type", h3(""), withSpinner(plotlyOutput("atdist_weap")))
+        )
+        
+    )
+), 
     
     tabItem(tabName = "page5",
             h2("Multivariate Insights into Global Terror"),
             fluidRow(
                 tabBox(
+                    title = "Casualty Count Distributions by different Dimensions",
+                    width = 12,
+                    tabPanel("By Year",h3(""), withSpinner(plotlyOutput("casdist_year"))),
+                    tabPanel("By Region",h3(""), withSpinner(plotlyOutput("casdist_region"))),
+                    tabPanel("By Attack Type",h3(""), withSpinner(plotlyOutput("casdist_attack"))),
+                    tabPanel("By Weapon Type",h3(""), withSpinner(plotlyOutput("casdist_weap")))
+                )),
+            fluidRow(
+                tabBox(
+                    title = "Detailed view on Attacks by Region and Year together with Attacktype used",
+                    width = 12,
+                    tabPanel("Attacks By Attacktype & Region", h3(""), withSpinner(plotlyOutput("dist_region2"))),
+                    tabPanel("Attacks By Region & Year", h3(""), withSpinner(plotlyOutput("distyear2")))
+                ) 
+            ),
+            fluidRow(
+                tabBox(
                     title = "Killings",
-                    width = "12",
+                    width = 12,
                     tabPanel("Year",h3("Overall killings per Year"), withSpinner(plotOutput('killings1')), includeMarkdown("killings1.md")),
                     tabPanel("Country",h3("Overall killings per Country"), withSpinner(plotOutput('killings2')), includeMarkdown("killings2.md")),
                     tabPanel("Countries & Years",h3("Killings per Countries and Years - size is proportional with the number of killings"), withSpinner(plotOutput('killings3')), includeMarkdown("killings3.md"))
@@ -212,6 +204,7 @@ body <- dashboardBody(tabItems(
             ),
             fluidRow(
                 tabBox(
+                    width = 12,
                     title = "Summary - Text Insights",
                     tabPanel("Wordcloud for Summary Text", withSpinner(plotOutput('word_cloud1'))),
                     tabPanel("Denogram for Summary Text", withSpinner(plotOutput('dendogram1')))
@@ -219,13 +212,13 @@ body <- dashboardBody(tabItems(
             )),
     tabItem(tabName = "page6",
             fluidRow(box(
-                width = "12",
+                width = 12,
                 h2("Top 1000 Cities by Attack Count"),
                 withSpinner(leafletOutput("map"))
             ),
             fluidRow(
                 box(
-                    width = "12",
+                    width = 12,
                     height = 1000,
                     title = "Globe View",
                     textOutput("globeText"),
